1b341a7faee6b10ba4cf57171c2a55c904499aaf
PapaKarlo787 <33232815+PapaKarlo787@users.noreply.github.com>
2021-08-12 23:14:14 +0500

    Update main.asm

--- main.asm
+++ main.asm
@@ -11,13 +11,13 @@ main:
 		call reverse_line
 	.lp:
 		gkey
-		cmp r15, 72
+		cmp r15, 117
 		je .up
-		cmp r15, 80
+		cmp r15, 114
 		je .down
-		cmp r15, 83
+		cmp r15, 115
 		je .del
-		cmp r15, 28
+		cmp r15, 90
 		jne .lp
 	mul r3, 19
 	movb r1, [r3+current_table+12]
